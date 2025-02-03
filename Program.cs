9debe8d06a8417a769dbfc52b7895835d30bd241
Janat Kazibwe <janat.kazibwe@mnsu.edu>
2025-02-02 22:53:04 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿class Clocktower
+{
+    static void Main()
+    {
+        //ask for user input
+        System.Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(System.Console.ReadLine());
+
+        //check if the number is even or odd
+        if (number % 2 == 0)
+        {
+            System.Console.WriteLine("Tick"); // Even number
+        }
+        else
+        {
+            System.Console.WriteLine("Tock"); // Odd number
+        }
+    }
+}
+
